98fba5d9b1efb73e60a5b2ba76c2a240793d3377
Adams <jtyjty99999@126.com>
2013-08-09 18:07:32 +0800

    使用zepto处理点击 修改页面切换方式为translate变换 使用classList与-webkit-overflow-scrolling:touch;

--- js/main.js
+++ js/main.js
@@ -20,6 +20,16 @@ window.ondeviceMotion = function(event){
     }
 
 }
+
+$('body').on('orientationchange',function(){
+
+    switch(window.orientation){
+        case 0:break;//竖屏
+        case -90:break;//左旋转
+        case 90:break;//右旋转
+        case 180:break;//横屏
+    }
+})
     /*
     * config
     *
@@ -33,7 +43,8 @@ var config ={
         port:'8080',
         path:'/mirrtalkApi/userInfo/index.php'
     },
-    '详细资料':'326baf'
+    '详细资料':'326baf',
+    'login':'53f689'
 }
 
 
@@ -90,24 +101,6 @@ for(;i<l;i++){
     },false);//hack来样式
 
 }
-var jumpers=document.querySelectorAll('.jumper');
-var len = jumpers.length,j=0;
-
-for(;j<len;j++){
-
-        jumpers[j].addEventListener('touchend',function(){
-            var title = $(this).attr('data-target');
-                jumpAndRender(title);//修复点击"闪一下"的bug(没渲染完毕?)
-            return false
-        },false);
-
-    jumpers[j].addEventListener('click',function(){
-        var title = $(this).attr('data-target');
-        jumpAndRender(title);//修复点击"闪一下"的bug(没渲染完毕?)
-        return false
-    },false);
-
-}
 
 
 function $ajaxRequest(option,doneCallback,fallCallback){
@@ -129,116 +122,3 @@ function $ajaxRequest(option,doneCallback,fallCallback){
     console.log(opt)
     $.ajax(opt)
 }
-
-function renderFromData(pageName,option,callback){
-    var box = 'article[data-pageName='+pageName+']';
-    $(box).bind('show',function(){
-            $ajaxRequest(option,function(msg){
-                callback.call(this,msg,pageName);
-            },function(jqXHR, textStatus){
-                throw new Error(textStatus)
-            })
-    })
-
-}
-
-var testData = {
-    userName:'18696770041',
-    agent:config.agent,
-    secret:config.secret,
-    serviceType:"326baf"
-}
-var info = {}
-renderFromData('详细资料',{
-type:'POST',
-url:'http://192.168.1.3:8080/mirrtalkApi/userInfo/getUserInfoAll.php',
-data:dictAndSha1ize(testData)
-},function(data,pageName){
-    console.log(data.RESULT);
-    var box = 'article[data-pageName='+pageName+']';
-    var tpl = document.querySelector(box).innerHTML;
-    var html = juicer(tpl, JSON.parse(data.RESULT));
-    console.log(html)
-    document.querySelector(box).innerHTML = html;
-    info = {
-        mirrtalkNumber:'18696770041',
-        agent:config.agent,
-        secret:config.secret,
-        serviceType:"326baf"
-    }
-    $('.confirm').on(activate, function () {
-        $ajaxRequest({
-            type:'POST',
-            url:'http://192.168.1.3:8080/mirrtalkApi/userInfo/updateUserInfo.php',
-            data:dictAndSha1ize(info)
-        },function(msg){
-            alert(msg)
-        },function(jqXHR, textStatus){
-            throw new Error(textStatus)
-        })
-    })
-
-
-})
-
-var showed='道客数读',prev=[];
-function jumpAndRender(pageName,override){
-var sel = 'article[data-pageName='+pageName+']';
-    var override = override || true;//若传入false，比如后退时，不需要计入访问队列。
-    document.querySelector('article[data-pageName='+showed+']').style.display='none';
-    document.querySelector(sel).style.display='block';
-    $('#title').text(pageName);
-    if(override){
-        var k=0,l=prev.length;
-        for(;k<l;k++){
-            if(prev[k]==showed){
-                prev.splice(k,1)
-                break
-            };
-        }
-        prev.push(showed);//维护一个访问记录队列，访问时计入队列。若队列中存在则不进入队列
-    }
-    showed = pageName;
-    $(sel).trigger('show')
-}
-
-
-document.querySelector('.leftBtn').addEventListener('touchend',function(){
-    prev.length!==0&&setTimeout(function(){
-        jumpAndRender(prev[prev.length-1],false);
-        prev.pop();
-    },0)
-},false);
-
-window.onload = function(){
-    document.querySelector('article[data-pageName='+showed+']').style.display='block'
-}
-
-
-
-$doc.delegate('.edit', activate, function () {
-    var _this = $(this);
-    var flag = _this.attr('editing'),type= _this.attr('editType') || 'text',param=_this.attr('param');
-    if (flag === 'false'||flag===undefined) {
-        var text = _this.text();
-        var boxWidth = parseInt(_this.width())-2;
-        var inner = '<input type='+type+' style="width:'+boxWidth/1.2+'px!important;padding:0px">';
-        _this.html(inner);
-        var input = _this.children('input');
-        input.val(text);
-        input.focus();
-        _this.attr('editing', 'true');
-        input.blur(function () {
-            var _this = $(this);
-            var text = _this.val();
-            var father = _this.parent();
-            father.html(text);
-            father.attr('editing', 'false');
-            father = null;
-            _this.unbind('blur');
-            _this = null;
-            info[param]=text;
-
-        })
-    }
-})
